1b650927fa42eac62bef9cd41b06ffdf75139aff
Christopher House <chhouse@microsoft.com>
2024-09-19 13:25:27 -0500

    Feature/redis dependency tracking (#6)
    * added dependency tracker for redis
    
    * updated test duration

---
+++ AdventureWorksApi/Services/RedisCacheDependencyTracker.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+
+
+namespace AdventureWorksApi.Services;
+
+public class RedisCacheDependencyTracker
+{
+    private readonly TelemetryClient _telemetryClient;
+
+    public RedisCacheDependencyTracker(TelemetryClient client)
+    {
+        _telemetryClient = client;
+    }
+
+    public async Task<T> ExecuteAndLogAsync<T>(Func<Task<T>> redisMethod, string opName)
+    {
+        using var operation = _telemetryClient.StartOperation<DependencyTelemetry>(opName);
+        operation.Telemetry.Name = "Redis";
+        operation.Telemetry.Type = "Redis";
+
+        try
+        {
+            return await redisMethod.Invoke();
+        }
+        catch
+        {
+            operation.Telemetry.Success = false;
+            throw;
+        }
+    }
+
+    public async Task ExecuteAndLogAsync(Func<Task> redisMethod, string opName)
+    {
+        using var operation = _telemetryClient.StartOperation<DependencyTelemetry>(opName);
+        operation.Telemetry.Name = "Redis";
+        operation.Telemetry.Type = "Redis";
+
+        try
+        {
+            await redisMethod.Invoke();
+        }
+        catch
+        {
+            operation.Telemetry.Success = false;
+            throw;
+        }
+    }
+}
