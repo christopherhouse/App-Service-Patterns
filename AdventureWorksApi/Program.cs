a5e9603a2b146b5eb97cee6bdbd82dc7da6a0cdb
Christopher House <chhouse@microsoft.com>
2024-09-19 10:10:57 -0500

    Feature/get customers (#3)
    * updated packages, added packages for ef, redis
    
    * added ef context, model, get customers initial implementation
    
    * Implemented caching for get customers

--- AdventureWorksApi/Program.cs
+++ AdventureWorksApi/Program.cs
@@ -1,9 +1,29 @@
+using System.Text.Json;
+using AdventureWorksApi.Data;
+using AdventureWorksApi.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AdventureWorksContext>(opt =>
+    opt.UseSqlServer(builder.Configuration["adventureWorksConnectionString"]));
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+{
+    var connectionString = builder.Configuration["redisConnectionString"];
+
+    if (string.IsNullOrEmpty(connectionString))
+    {
+        throw new ArgumentException("Redis connection string was not found");
+    }
+
+    return ConnectionMultiplexer.Connect(connectionString);
+});
 
 var app = builder.Build();
 
@@ -16,29 +36,29 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/api/customers", async (AdventureWorksContext db, IConnectionMultiplexer redis) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var redisDb = redis.GetDatabase();
+    var key = "/api/customers";
+    List<Customer> customerList = null!;
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    var cachedCustomers = await redisDb.StringGetAsync(key);
+
+    if (cachedCustomers.IsNullOrEmpty)
+    {
+        customerList = await db.Customers.Take(5).ToListAsync();
+        await redisDb.StringSetAsync(key, JsonSerializer.Serialize(customerList), TimeSpan.FromMinutes(1));
+    }
+    else
+    {
+        customerList = JsonSerializer.Deserialize<List<Customer>>(cachedCustomers!)!;
+    }
+    
+    return customerList;
+
+}).WithName("GetCustomers")
+  .WithOpenApi();
+
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
